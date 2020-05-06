8699e6aeae9c8c8b985252dbd6da64ac7ff6f45a
zyhou <richard.maxime9@gmail.com>
2020-05-06 22:45:39 +0200

    fix:  hooks rules

--- react.js
+++ react.js
@@ -1,5 +1,10 @@
 module.exports = {
-    extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended'],
+    plugins: ['react-hooks'],
+    extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+    },
     settings: {
         react: {
             version: 'detect',
