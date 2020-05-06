ef0bc8970083ce8483e4341b6a965335a3b036a7
zyhou <richard.maxime9@gmail.com>
2020-05-06 22:22:22 +0200

    chore(release): 0.0.5

--- react.js
+++ react.js
@@ -1,10 +1,5 @@
 module.exports = {
-    plugins: ['react-hooks'],
-    extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
-    rules: {
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-    },
+    extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended'],
     settings: {
         react: {
             version: 'detect',
